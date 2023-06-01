805992e3116e1d785362ecc2aa7ecfe5f98a5e79
hoshinora22 <1397466995@qq.com>
2023-06-01 17:46:54 +0800

    添加矩阵struct

--- main.cu
+++ main.cu
@@ -1,11 +1,17 @@
 #include <iostream>
+#include "src/MathUtils.h"
 
-__global__ void hi_gpu() {
+__global__ void hi_gpu()
+{
     printf("Hello World from GPU!\n");
 }
 
-int main() {
+
+int main()
+{
+    // 用__global__定义的kernel是异步的，这意味着host不会等待kernel执行完就执行下一步
     hi_gpu<<<1, 1>>>();
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
---
+++ src/MathUtils.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nora on 2023-6-1.
+//
+
+#include "MathUtils.h"
